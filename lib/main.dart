88103ad115acd7667ef07f577fd4964ec56e1956
zeruFili <zerubabeldagilat@gmail.com>
2024-10-29 21:18:31 +0300

    Add margin and padding adjustments

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,46 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
       home: Scaffold(
-    appBar: AppBar(
-      title: Text("i am poor"),
-      backgroundColor: Colors.grey,
-    ),
-    body: Container(
-      child: Image(
-        image: AssetImage(
-            'images/nationalid.png'), // Ensure the asset is correctly loaded
-      ),
-    ),
-  )));
+          backgroundColor: Colors.teal,
+          body: SafeArea(
+            child: Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment
+                    .spaceBetween, // Correctly set mainAxisAlignment
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Container(
+                    height: double.infinity,
+                    width: 100.0,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    margin:
+                        EdgeInsets.symmetric(vertical: 200.0, horizontal: 0.0),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          color: Colors
+                              .yellow, // Use Colors.yellow instead of Color(yellow)
+                        ),
+                        Container(
+                          height: 100.0,
+                          width: 100.0,
+                          color: Colors
+                              .green, // Use Colors.yellow instead of Color(yellow)
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: double.infinity,
+                    width: 100.0,
+                    color: Colors.lightBlue,
+                  )
+                ],
+              ),
+            ),
+          ))));
 }
