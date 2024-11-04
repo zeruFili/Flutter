7d80ae5cf0f3509a9a7f787df787819b5dc6758a
zeruFili <zerubabeldagilat@gmail.com>
2024-11-04 17:25:47 +0300

    Add completed components onPressed and setState  and functionalities

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,62 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.teal,
-          body: SafeArea(
-            child: Container(
-              child: Column(
-                mainAxisAlignment:
-                    MainAxisAlignment.center, // Correctly set mainAxisAlignment
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('DAgilat faya'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
 
-                children: [
-                  CircleAvatar(
-                    radius: 50.0,
-                    backgroundImage: AssetImage("images/nationalid.png"),
-                  ),
-                  Text(
-                    'Zerubabel Filmon ',
-                    style: TextStyle(
-                        fontFamily: 'Pacifico',
-                        fontSize: 40.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                  Text(
-                    'Flutter Devleoper today ',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      color: Colors.white,
-                      fontFamily: 'Source San 3',
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                    width: 150.0,
-                    child: Divider(
-                      color: Colors.teal.shade100,
-                    ),
-                  ),
-                  Card(
-                    color: Colors.white,
-                    // Padding around the container
-                    margin: EdgeInsets.symmetric(
-                        vertical: 10.0,
-                        horizontal: 25.0), // Corrected margin syntax
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.add_ic_call,
-                        size: 20.0, // Size of the icon
-                        color: Colors
-                            .teal, // Changed to black for visibility against white background
-                      ),
-                      title: Text(
-                        '+251 951383935',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors
-                              .black, // Changed to black for visibility against white background
-                          fontFamily:
-                              'Source Sans Pro', // Corrected font family name
-                        ),
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20.0,
-                  ),
-                  Card(
-                    color: Colors.white, // Background color of the container
-                    // Padding around the container
-                    margin: EdgeInsets.symmetric(
-                      // Margin around the container
-                      vertical: 10.0,
-                      horizontal: 20.0,
-                    ),
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.email,
-                        size: 20.0, // Size of the icon
-                        color: Colors.teal, // Icon color
-                      ),
-                      title: Text(
-                        'dagilat@gmail.com',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          color: Colors.black, // Text color
-                          fontFamily: 'Source Sans Pro', // Font family
-                        ),
-                      ),
-                    ),
-                  )
-                ],
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftNumber = 1;
+  var rightNumber = 1;
+
+  void rollRDice() {
+    setState(() {
+      rightNumber = Random().nextInt(6) + 1;
+      leftNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: rollRDice,
+              child: Padding(
+                padding: EdgeInsets.all(16.0), // Add padding here
+                child: Image.asset('images/dice$leftNumber.png'),
               ),
             ),
-          ))));
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: rollRDice,
+              child: Padding(
+                padding: EdgeInsets.all(16.0), // Add padding here
+                child: Image.asset('images/dice$rightNumber.png'),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
